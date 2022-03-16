6c85023b1d783691214605af7050c12b2aa52339
Adam <adongup@163.com>
2022-03-16 19:49:24 +0800

    master commit

--- src/main/java/com/zhangxd/maven/Calculator.java
+++ src/main/java/com/zhangxd/maven/Calculator.java
@@ -9,6 +9,6 @@ public class GitTest{
     public static void main(String[] args) {
         System.out.println("hello git!");
         System.out.println("hello,Git!");
-        System.out.println("hello,Git!1");
+        System.out.println("master test!");
     }
 }
